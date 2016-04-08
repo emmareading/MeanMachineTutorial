37e3bd9de16bf1b1bb2d259697d2a4ac6ca0bb7b
Emma Reading <readinge@uvic.ca>
2016-04-08 11:02:00 -0700

    Added files via upload
    routing-app 
    successfully completed "Starting Angular" chapter

--- server.js
+++ server.js
@@ -1,63 +1,16 @@
-// BASE SETUP
-// =====================
+// get the things we need
+var express = require('express');
+var app     = express();
+var path = require('path');
 
-// CALL PACKAGES -----------
-var express = require('express'); //call express
-var app = express(); //define our app using express
-var bodyParser = require('body-parser'); //get body parser
-var morgan = require('morgan'); //used to see requests
-var mongoose = require('mongoose'); //for working with our database
-var User = require('./models/user');
-var port = process.env.PORT || 8080; //set the port for our app
+// set the public folder to serve public assets
+app.use(express.static(__dirname + '/public'));
 
-//connect to our database (hosted on modulus.io)
-mongoose.connect('mongodb://node:noder@novus.modulusmongo.net:27017/Iganiq8o');
-
-
-// APP CONFIGURATION ----------
-
-// use body parder so we can grab information from POST requests
-app.use(bodyParser.urlencoded({entended: true}));
-app.use(bodyParser.json());
-
-//configure our app to handle CORS requests
-app.use(function(req, res,next) {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, \
-	Authorization');
-	next();
+// set up our one route to the index.html file
+app.get('*', function(req, res) {
+	res.sendFile(path.join(__dirname + '/public/views/index.html'));
 });
 
-// log all requests to the console
-
-app.use(morgan('dev'));
-
-// ROUTES FOR OUR API
-// ======================
-
-// basic route for the home page
-app.get('/', function(req, res) {
-	res.send('Welcome to the home page!');
-});
-
-//get an instance of the express router
-var apiRouter = express.Router();
-
-//test route to make sure everything is workin
-//accessed at GET http://localhost:8080/api
-
-apiRouter.get('/', function(req,res) {
-	res.json({message: 'hooray! welcome to our api!'})
-});
-
-//more routes for our API will happen here
-
-//REGISTER OUR ROUTES -------------
-// all of our routes will be prefixed with /api
-app.use('/api', apiRouter);
-
-//START THE SERVER
-// ====================
-app.listen(port);
-console.log('Magin happes on port ' + port);
+// start the server on port 8080 (http://localhost:8080)
+app.listen(8080);
+console.log('Magic happens on port 8080.');
